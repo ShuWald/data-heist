aa386b64b66d098bb35d105211043a1a18f65404
karlklaffen <karlklaffen@gmail.com>
2026-04-04 18:14:02 -0700

    cleaned up preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,16 @@ from functions import FileData
 import pandas as pd
 
 useful_data = FileData('ConsumerData.csv')
-useless_columns = ['RecordID', 'MAK', 'BaseMak', 'Address', 'City', 'State', 'Zipcode']
-useful_data = useful_data.dataframe.drop(columns=useless_columns)
+
+df = useful_data.dataframe
+
+df.loc[:, 'Address'] = df.loc[:, 'Address'].apply(lambda address: address[address.find(' ') + 1:])
+
+useless_columns = ['RecordID', 'MAK', 'BaseMak', 'City', 'State', 'Zipcode', 'Latitude', 'Longitude']
+useful_data = df.drop(columns=useless_columns)
 details = useful_data.columns
+
 print(details)
 print(useful_data.to_numpy().flatten())
+
+# useful_data.to_csv('clean_consumer_data.csv', index = False)
